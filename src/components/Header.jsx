7fb0101752a6bf25fd4454fa3fde71a4abdce64d
Anishu99 <amaini67@gmail.com>
2026-01-25 18:38:41 +0530

    Implemented Search Functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { APPLICATION_LOGO_URL } from "../../utils/constant";
 
 export const Header = () => {
+  const [buttonName, setButtonName] = useState("Logout");
   return (
     <div className="header">
       <div className="logo-container">
@@ -11,6 +13,16 @@ export const Header = () => {
           <li>Home</li>
           <li>About Us</li>
           <li>Cart</li>
+          <button
+            className="logout-button"
+            onClick={() => {
+              buttonName === "Logout"
+                ? setButtonName("Login")
+                : setButtonName("Logout");
+            }}
+          >
+            {buttonName}
+          </button>
         </ul>
       </div>
     </div>
--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -4,18 +4,21 @@ import { API_URL_FOR_FETCH_RESTAURANT_LIST } from "../../../utils/constant";
 import RestaurantSkeleton from "./RestaurantSkeleton";
 
 const Body = () => {
+  const [filteredRestaurantList, setFilteredRestaurantList] = useState([]);
   const [restaurantList, setRestaurantList] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
   useEffect(() => {
     fetchRestaurantList();
   }, []);
 
   const fetchRestaurantList = async () => {
-    const data = await fetch(API_URL_FOR_FETCH_RESTAURANT_LIST);
-    const data_to_json = await data.json();
+    const api_response = await fetch(API_URL_FOR_FETCH_RESTAURANT_LIST);
+    const data_to_json = await api_response.json();
     let restaurantResponse =
       data_to_json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants;
     setRestaurantList(restaurantResponse);
+    setFilteredRestaurantList(restaurantResponse);
   };
 
   return restaurantList.length === 0 ? (
@@ -23,10 +26,31 @@ const Body = () => {
   ) : (
     <div className="body">
       <div className="filter">
+        <input
+          type="text"
+          className="search-box"
+          value={searchValue}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+          }}
+        ></input>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRestaurants = restaurantList.filter((restaurant) =>
+              restaurant.info.name
+                .toLowerCase()
+                .includes(searchValue.toLowerCase())
+            );
+            setFilteredRestaurantList(filteredRestaurants);
+          }}
+        >
+          Search
+        </button>
         <button
           onClick={() => {
-            setRestaurantList(
-              restaurantList.filter((res) => res.info.avgRating >= 4.5)
+            setFilteredRestaurantList(
+              filteredRestaurantList.filter((res) => res.info.avgRating >= 4.5)
             );
           }}
           className="filter-btn"
@@ -36,14 +60,14 @@ const Body = () => {
         <button
           className="filter-btn"
           onClick={() => {
-            fetchRestaurantList();
+            setFilteredRestaurantList(restaurantList);
           }}
         >
           Reset
         </button>
       </div>
       <div className="restaurant-container">
-        {restaurantList.map((resData) => {
+        {filteredRestaurantList.map((resData) => {
           return (
             <RestaurantCard
               key={resData.info.id}
